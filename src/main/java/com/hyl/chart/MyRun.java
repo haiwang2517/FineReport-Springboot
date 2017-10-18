0a07ea4e098f7d9c3253e52ef5764eb3b04cee77
wobunengmeiyou_ni <wobunengmeiyou_ni@163.com>
2017-10-18 11:21:23 +0800

    调整目录,删除没用的东西

--- src/main/java/com/hyl/chart/MyRun.java
+++ src/main/java/com/hyl/chart/MyRun.java
@@ -9,12 +9,6 @@ package com.hyl.chart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-import com.fr.web.ReportServlet;
 
 /**
  * 
@@ -24,20 +18,9 @@ import com.fr.web.ReportServlet;
  * @since 0.1.0
  */
 @SpringBootApplication
-public class MyRun extends WebMvcConfigurerAdapter {
+public class MyRun { // extends WebMvcConfigurerAdapter
 
-	@Bean
-	RestTemplate restTemplate(){
-		return new RestTemplate();
-	}
 	public static void main(String[] args) {
 		SpringApplication.run(MyRun.class, args);
 	}
-
-	@Bean
-	public ServletRegistrationBean ServletRegistrationBean() {
-		// 报表组件 
-		return new ServletRegistrationBean(new ReportServlet(), "/ReportServer");
-	}
-
 }
---
+++ src/main/java/com/hyl/chart/config/ReportServletConfig.java
@@ -0,0 +1,33 @@
+/**   
+ *
+ * @author HYL   
+ * @date 2017年10月18日 上午11:14:58
+ * @version 0.1.0
+ * @since 0.1.0  
+ */
+package com.hyl.chart.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.fr.web.ReportServlet;
+
+/**   
+ * 
+ * @author HYL
+ * @date 2017年10月18日 上午11:14:58
+ * @version 0.1.0
+ * @since 0.1.0  
+ */
+@Configuration
+public class ReportServletConfig {
+
+	@Bean
+	public ServletRegistrationBean ServletRegistrationBean() {
+		// 报表组件 
+		System.out.println("****************/ReportServer");
+		return new ServletRegistrationBean(new ReportServlet(), "/ReportServer");
+	}
+	
+}
